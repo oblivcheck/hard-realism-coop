ccdb9e5d4c41c17a6f4aaaac1b359dfdaca195cb
oblivcheck <rertna.anya@gmail.com>
2024-07-15 21:12:46 +0800

    shell

---
+++ run_hrc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /tmp/hard-realism-coop/
+git pull
+cp -r server/* /home/steam/Steam/hrc/
+cp -r run_hrc.sh /home/steam/Steam/
+chmod +x /home/steam/Steam/run_hrc.sh
+
+screen -S h1 /home/steam/Steam/hrc/srcds_run +map c8m1_apartment -port 37010 -tickrate 96
